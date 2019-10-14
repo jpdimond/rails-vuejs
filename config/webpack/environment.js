8168a24844b64fc8cbe94338e2b004316f9d502c
Jill Dimond <jill@sassafras.coop>
2019-10-14 11:56:44 -0400

    added rails erb loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
+const erb =  require('./loaders/erb')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('erb', erb)
+
 module.exports = environment
